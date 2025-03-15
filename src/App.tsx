b998ebe81ae4f12f170f49e4e30c9dd1edb0ba83
shiva prasad <agustya742@gmail.com>
2025-03-15 14:37:39 +0530

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { BrowserRouter, createBrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Bannerblock from './components/Bannerblock'
 import Footer from './components/footer'
